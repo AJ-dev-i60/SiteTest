1b24729c6c17302dbfe01c73a4efccb1dc3ecc15
AJ-dev-i60 <armandt@isixty.co.za>
2025-08-19 13:10:12 +0200

    Create action.js

---
+++ action.js
@@ -0,0 +1,4 @@
+document.getElementById("clickMeBtn").addEventListener("click", function() {
+    const message = document.getElementById("message");
+    message.textContent = "Button clicked! 🎉 GitHub Pages is working!";
+});
